797d38732a716ae44385622b6f51be96b523eb57
yvendee <71810479+yvendee@users.noreply.github.com>
2024-04-11 22:07:52 +0800

    Add files via upload
    Initial commit

---
+++ autopir/autopir.ino
@@ -0,0 +1,47 @@
+int relay = 13;
+int sensor = 2;
+
+unsigned long previousMillis = 0;    // Stores the last time the variable was updated
+unsigned long interval = 1000;       // Interval (in milliseconds) at which the variable will increment
+
+int minute = 0;
+int second = 0;
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();  // Get the current time
+
+  // Check if it's time to increment the variable and print
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // Save the last time the variable was updated
+    second++;             // Increment the variable
+  }
+
+  // Check if 2 minutes have passed, then execute the action
+  if (second >= 60) {
+    Serial.println("Minute passed");
+    minute++;
+    second = 0;
+  }
+
+  if (minute >= 2) {
+    Serial.println("Deactivate relay");
+    // will turn off the relay
+    digitalWrite(relay, HIGH);
+    minute = 0;
+  }
+
+
+  int val = digitalRead(sensor);
+  if (val == HIGH) {    // detect movement
+    Serial.println("Activate relay");
+    digitalWrite(relay, LOW);
+    second = 0;
+    minute = 0;
+  }
+}
