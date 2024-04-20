88dbf34fcab6fa4d6f69a9a4dfdc6ff53a0bf484
yvendee <71810479+yvendee@users.noreply.github.com>
2024-04-21 07:06:57 +0800

    Update multipir.ino

--- multipir/multipir/multipir.ino
+++ multipir/multipir/multipir.ino
@@ -2,7 +2,8 @@ int relay = 13;
 int sensor = 3; // outside sensor
 int sensor1 = 4; // inside sensor
 int sensor2 = 5; // middle sensor
-int mode = 0;
+volatile int mode = 0;
+
 
 unsigned long previousMillis = 0;    // Stores the last time the variable was updated
 unsigned long interval = 1000;       // Interval (in milliseconds) at which the variable will increment
@@ -14,14 +15,14 @@ int people = 0;
 void setup() {
   pinMode(relay, OUTPUT);
   pinMode(sensor, INPUT);
-  pinMode(sensor1,INPUT);
-  pinMode(sensor2,INPUT);
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
   Serial.begin(9600);
   digitalWrite(relay, HIGH);
-
 }
 
 void loop() {
+
   unsigned long currentMillis = millis();  // Get the current time
 
   // Check if it's time to increment the variable and print
@@ -34,46 +35,55 @@ void loop() {
 
   // Check if 2 minutes have passed, then execute the action
   if (second >= 60) {
-    Serial.println("Minute passed");
+    // Serial.println("Minute passed");
     minute++;
     second = 0;
   }
 
   if (minute >= 1) {
-
     minute = 0;
   }
 
-
-  int val = digitalRead(sensor);     // outside sensor
-  int val1 = digitalRead(sensor1)    // inside sensor
-  int val2 = digitalRead(sensor2)    //  middle sensor
+  bool val = digitalRead(sensor) == HIGH;     // outside sensor
+  bool val1 = digitalRead(sensor1) == HIGH;   // inside sensor
+  bool val2 = digitalRead(sensor2) == HIGH;   // middle sensor
 
   if (mode == 0) {        // first scene
-    if (val == HIGH) {    // first, detect any movement from outside sensor or
+    if (val) {    // first, detect any movement from outside sensor or
+    
       mode = 1;
+      Serial.println("outside-detect");
     }
 
-    if (va1l == HIGH) {    // detect any movement from insides sensor
+    if (val1) {    // detect any movement from insides sensor
       mode = 10;
+      Serial.println("inside-detect");
     }
   }
 
 
   if (mode == 1) {
-    if (val2 == HIGH) {    // if outside sensor has movement, wait to detect movement from middle sensor
+    if (val2) {    // if outside sensor has movement, wait to detect movement from middle sensor
      mode = 2;
     }
   }
 
    if (mode == 2) {
-    if (va1l == HIGH) {    // if middle sensor has movement, wait to detect movement from inside sensor
+    if (val1) {    // if middle sensor has movement, wait to detect movement from inside sensor
       mode = 3;
     }
    }
 
-   if (mode == 3) {
+   if(mode == 3){
+      if (val1 == false) {    // if inside sensor has movement, wait for inside sensor to get low
+        mode = 4;
+      }
+   }
+
+
+   if (mode == 4) {
       people++;
+      Serial.println("count");
 
       if(people > 200) {
         people = 200;
@@ -83,33 +93,42 @@ void loop() {
 
 
    if(mode == 10) {
-      if (val2 == HIGH) {    // if inside sensor has movement, wait to detect movement from middle sensor
+      if (val2) {    // if inside sensor has movement, wait to detect movement from middle sensor
        mode = 12;
       }
    }
 
   if (mode == 12) {
-    if (va1 == HIGH) {    // if middle sensor has movement, wait to detect movement from outside sensor
+    if (val) {    // if middle sensor has movement, wait to detect movement from outside sensor
       mode = 13;
     }
   }
 
-  if (mode == 13) {
+  if(mode == 13){
+    if (val == false) {    // if outside sensor has movement, wait for outside sensor to get low
+      mode = 14;
+    }
+  }
+
+  if (mode == 14) {
       people--;
+      // Serial.println("count");
+
       if(people < 0) {
         people = 0;
       }
       mode = 0;            // go back to first scene
-   }
+  }
 
-   if (people > 0) {
-     digitalWrite(relay, LOW); // activate relay
-     mode = 0;
-   }
+
 
   if (people == 0) {
-     digitalWrite(relay, HIGH); // deactivate relay
-     mode = 0;
-   }
+    // Serial.println("relay is off");
+    digitalWrite(relay, HIGH); // deactivate relay
+  }
+  else {
+    // Serial.println("relay is on");
+    digitalWrite(relay, LOW); // activate relay
+  }
 
 }
